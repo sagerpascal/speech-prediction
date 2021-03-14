488dc8e330697240e9ccf20854e1bf808339e2a7
Pascal Sager <sage@zhaw.ch>
2021-03-14 11:04:26 +0100

    Cleanup code

--- src/datasets/preprocessing.py
+++ src/datasets/preprocessing.py
@@ -24,11 +24,11 @@ def get_mfcc_transform(conf):
     return mfcc_transform
 
 
-class PreprocessEnd(nn.Module):
+class Preprocess(nn.Module):
     """ must be a class, otherwise multiprocessing won't work """
 
     def __init__(self, n_frames, k_frames, use_random_pos):
-        super(PreprocessEnd, self).__init__()
+        super(Preprocess, self).__init__()
         self.n_frames = n_frames
         self.k_frames = k_frames
         self.use_random_pos = use_random_pos
@@ -37,96 +37,82 @@ class PreprocessEnd(nn.Module):
 
     def forward(self, mfcc):
         if mfcc.shape[2] < self.k_frames:
-            # mfcc contains less frames than we want to predict
+            # mfcc contains less frames than we want to predict -> set data and target to 0
             data = torch.zeros((mfcc.shape[0], mfcc.shape[1], self.n_frames), dtype=torch.float)
             target = torch.zeros((mfcc.shape[0], mfcc.shape[1], self.k_frames), dtype=torch.float)
             if self.ignored_count % 500:
-                logger.debug("{} MFCCs were smaller than frames we want to predict... ignored them".format(self.ignored_count))
+                logger.debug(
+                    "{} MFCCs were smaller than frames we want to predict... ignored them".format(self.ignored_count))
             self.ignored_count += 1
+            return data, target
 
         elif mfcc.shape[2] <= self.n_frames + self.k_frames:
-            target_index = mfcc.shape[2] - self.k_frames
-            data = mfcc.detach().clone()[:, :, :target_index]
-            target = mfcc.detach().clone()[:, :, target_index:]
             if self.too_small_count % 1000:
                 logger.debug("{} MFCCs were smaller than n_frames+k_frames...".format(self.too_small_count))
             self.too_small_count += 1
+            return self.forward_action_to_small(mfcc)
 
         else:
-            # Select a random section of the MFCC, the first part is used as data x and the second as target y
-            # use a random segment of the length n_frames + k_frames
-            start_index = random.randint(0, mfcc.shape[2] - (self.n_frames + self.k_frames + 1)) if self.use_random_pos else 0
-            data = mfcc.detach().clone()[:, :, start_index:start_index + self.n_frames]
-            target = mfcc.detach().clone()[:, :,
-                     start_index + self.n_frames:start_index + self.n_frames + self.k_frames]
+            start_index = random.randint(0, mfcc.shape[2] - (self.n_frames + self.k_frames + 1)) if self.use_random_pos\
+                else 0
+            return self.forward_action(mfcc, start_index)
+
+    def forward_action_too_small(self, mfcc):
+        pass
+
+    def forward_action(self, mfcc, start_index):
+        pass
+
+
+class PreprocessEnd(Preprocess):
+
+    def forward_action_too_small(self, mfcc):
+        target_index = mfcc.shape[2] - self.k_frames
+        data = mfcc.detach().clone()[:, :, :target_index]
+        target = mfcc.detach().clone()[:, :, target_index:]
+        return data, target
+
+    def forward_action(self, mfcc, start_index):
+        # Select a random section of the MFCC, the first part is used as data x and the second as target y
+        # use a random segment of the length n_frames + k_frames
+        data = mfcc.detach().clone()[:, :, start_index:start_index + self.n_frames]
+        target = mfcc.detach().clone()[:, :, start_index + self.n_frames:start_index + self.n_frames + self.k_frames]
         return data, target
 
 
-class PreprocessBeginning(nn.Module):
-    """ must be a class, otherwise multiprocessing won't work """
+class PreprocessBeginning(Preprocess):
 
-    def __init__(self, n_frames, k_frames, use_random_pos):
-        super(PreprocessBeginning, self).__init__()
-        self.n_frames = n_frames
-        self.k_frames = k_frames
-        self.use_random_pos = use_random_pos
+    def forward_action_too_small(self, mfcc):
+        target = mfcc.detach().clone()[:, :, :self.k_frames]
+        data = mfcc.detach().clone()[:, :, self.k_frames:]
+        return data, target
 
-    def forward(self, mfcc):
-        if mfcc.shape[2] < self.k_frames:
-            # mfcc contains less frames than we want to predict
-            data = torch.zeros((mfcc.shape[0], mfcc.shape[1], self.n_frames), dtype=torch.float)
-            target = torch.zeros((mfcc.shape[0], mfcc.shape[1], self.k_frames), dtype=torch.float)
-            logger.info("MFCC is smaller than frames we want to predict... ignore it")
-
-        elif mfcc.shape[2] <= self.n_frames + self.k_frames:
-            target = mfcc.detach().clone()[:, :, :self.k_frames]
-            data = mfcc.detach().clone()[:, :, self.k_frames:]
-            logger.debug("MFCC is smaller than n_frames+k_frames...")
-
-        else:
-            # Select a random section of the MFCC, the first part is used as target y and the second as data x
-            # use a random segment of the length n_frames + k_frames
-            start_index = random.randint(0, mfcc.shape[2] - (self.n_frames + self.k_frames + 1)) if self.use_random_pos else 0
-            target = mfcc.detach().clone()[:, :, start_index:start_index + self.k_frames]
-            data = mfcc.detach().clone()[:, :,
-                   start_index + self.k_frames:start_index + self.k_frames + self.n_frames]
+    def forward_action(self, mfcc, start_index):
+        # Select a random section of the MFCC, the first part is used as target y and the second as data x
+        # use a random segment of the length n_frames + k_frames
+        target = mfcc.detach().clone()[:, :, start_index:start_index + self.k_frames]
+        data = mfcc.detach().clone()[:, :, start_index + self.k_frames:start_index + self.k_frames + self.n_frames]
         return data, target
 
 
-class PreprocessCenter(nn.Module):
-    """ must be a class, otherwise multiprocessing won't work """
+class PreprocessCenter(Preprocess):
 
-    def __init__(self, n_frames, k_frames, use_random_pos):
-        super(PreprocessCenter, self).__init__()
-        self.n_frames = n_frames
-        self.k_frames = k_frames
-        self.use_random_pos = use_random_pos
+    def forward_action_too_small(self, mfcc):
+        # Data: 0 -> n1 and n1+k_frames -> end
+        # Target: n1 -> n1+k_frames
+        n1 = (mfcc.shape[2] - self.k_frames) // 2
+        target = mfcc.detach().clone()[:, :, n1:n1 + self.k_frames]
+        data = torch.cat((mfcc.detach().clone()[:, :, :n1], mfcc.detach().clone()[:, :, n1 + self.k_frames:]), 2)
+        return data, target
 
-    def forward(self, mfcc):
-        if mfcc.shape[2] < self.k_frames:
-            # mfcc contains less frames than we want to predict
-            data = torch.zeros((mfcc.shape[0], mfcc.shape[1], self.n_frames), dtype=torch.float)
-            target = torch.zeros((mfcc.shape[0], mfcc.shape[1], self.k_frames), dtype=torch.float)
-            logger.info("MFCC is smaller than frames we want to predict... ignore it")
-
-        elif mfcc.shape[2] <= self.n_frames + self.k_frames:
-            # Data: 0 -> n1 and n1+k_frames -> end
-            # Target: n1 -> n1+k_frames
-            n1 = (mfcc.shape[2] - self.k_frames) // 2
-            target = mfcc.detach().clone()[:, :, n1:n1 + self.k_frames]
-            data = torch.cat((mfcc.detach().clone()[:, :, :n1], mfcc.detach().clone()[:, :, n1 + self.k_frames:]), 2)
-            logger.debug("MFCC is smaller than n_frames+k_frames...")
-
-        else:
-            # Data: start_index -> start_index+n1 and start_index+n1+k_frames -> start_index+n_frames+k_frames
-            # Target: start_index+n1 -> start_index+n1+k_frames
-            start_index = random.randint(0, mfcc.shape[2] - (self.n_frames + self.k_frames + 1)) if self.use_random_pos else 0
-            n1 = self.n_frames // 2
-            target = mfcc.detach().clone()[:, :, start_index + n1:start_index + n1 + self.k_frames]
-            data = torch.cat((mfcc.detach().clone()[:, :, start_index:start_index + n1],
-                              mfcc.detach().clone()[:, :,
-                              start_index + n1 + self.k_frames:start_index + self.k_frames + self.n_frames]),
-                             2)
+    def forward_action(self, mfcc, start_index):
+        # Data: start_index -> start_index+n1 and start_index+n1+k_frames -> start_index+n_frames+k_frames
+        # Target: start_index+n1 -> start_index+n1+k_frames
+        n1 = self.n_frames // 2
+        target = mfcc.detach().clone()[:, :, start_index + n1:start_index + n1 + self.k_frames]
+        data = torch.cat((mfcc.detach().clone()[:, :, start_index:start_index + n1],
+                          mfcc.detach().clone()[:, :,
+                          start_index + n1 + self.k_frames:start_index + self.k_frames + self.n_frames]), 2)
 
         return data, target
 

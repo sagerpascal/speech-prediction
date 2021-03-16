93dcb9d6b928d1a0eb677bfe559f4011bc37f845
Pascal Sager <paes.sager@gmail.com>
2021-03-16 14:01:50 +0100

    Plot some data for validation

--- src/datasets/dataset_h5.py
+++ src/datasets/dataset_h5.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from torch.utils.data import Dataset
 from datasets.preprocessing import get_mfcc_preprocess_fn
 import pandas as pd
-
+import torchaudio
 
 class AudioDatasetH5(Dataset):
 
-    def __init__(self, conf, mode, df_base_path='/workspace/data_pa/'):
+    def __init__(self, conf, mode, df_base_path='/workspace/data_pa/', with_waveform=False):
 
         df_base_path = Path(df_base_path)
         meta_base_path = Path('datasets/dfs')
@@ -35,6 +35,7 @@ class AudioDatasetH5(Dataset):
         self.k_frames = conf['masking']['k_frames']
         self.n_frames = conf['masking']['n_frames']
         self.sliding_window = conf['masking']['start_idx'] == 'sliding-window'
+        self.with_waveform = with_waveform
 
         # ignore all files < k_frames + n_frames
         self.metadata_df = self.metadata_df[self.metadata_df['MFCC_length'] >= (self.n_frames + self.k_frames)]
@@ -69,6 +70,12 @@ class AudioDatasetH5(Dataset):
             mfcc_start = self.metadata_df['MFCC_start'][index_dataframe]
             mfcc_end = self.metadata_df['MFCC_end'][index_dataframe]
 
+        if self.with_waveform:
+            file = self.metadata_df['file_path'][index_dataframe]
+            waveform = torchaudio.load(file)[0]
+        else:
+            waveform = 0
+
         speaker = self.metadata_df['Speaker'][index_dataframe]
         mfcc = self.h5_file['MFCC'][:, :, mfcc_start:mfcc_end]
         # mfcc_start, mfcc_end = self.h5_file['META'][item]
@@ -78,10 +85,41 @@ class AudioDatasetH5(Dataset):
         mfcc = torch.from_numpy(mfcc)
         data, target = self.preprocess(mfcc)
 
-        return data, target, mfcc, None, speaker
+        return data, target, mfcc, waveform, speaker
 
     def __len__(self):
         return self.dataset_length
 
     def __del__(self):
         self.h5_file.close()
+
+
+def get_some_data():
+    import random
+    from utils.conf_reader import get_config
+    import scipy.io.wavfile
+    from librosa.feature.inverse import mfcc_to_audio
+
+    conf = get_config()
+    dataset = AudioDatasetH5(conf, 'train', with_waveform=True)
+
+    for i in range(5):
+        idx = random.randint(0, len(dataset))
+        data, target, mfcc, waveform = dataset[idx]
+
+        data = data.cpu().numpy()
+        target = target.cpu().numpy()
+        mfcc = mfcc.cpu().numpy()
+
+        scipy.io.wavfile.write('waveform_{}.wav'.format(i), conf['data']['transform']['sample_rate'], waveform.T)
+        scipy.io.wavfile.write('MFCC_{}.wav'.format(i), conf['data']['transform']['sample_rate'],
+                               mfcc_to_audio(mfcc.T, hop_length=conf['data']['transform']['hop_length']))
+        scipy.io.wavfile.write('data_{}.wav'.format(i), conf['data']['transform']['sample_rate'],
+                               mfcc_to_audio(data.T, hop_length=conf['data']['transform']['hop_length']))
+        scipy.io.wavfile.write('target_{}.wav'.format(i), conf['data']['transform']['sample_rate'],
+                               mfcc_to_audio(target.T, hop_length=conf['data']['transform']['hop_length']))
+
+
+if __name__ == '__main__':
+    get_some_data()
+

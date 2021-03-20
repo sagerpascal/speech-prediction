c334709552131ac17c7da7fe974e9fe6a051d104
Pascal Sager <sage@zhaw.ch>
2021-03-20 14:32:48 +0100

    cleanup loading parameters

--- src/models/model.py
+++ src/models/model.py
@@ -15,31 +15,35 @@ logger = logging.getLogger(__name__)
 
 
 def get_model(conf, device):
-    if 'load_model' in conf and conf['load_model'] != 'None':
-        try:
-            model = torch.load("data/trained_models/{}.pth".format(conf['load_model']),
-                               map_location=torch.device(device))
-        except FileNotFoundError:
-            try:
-                model = torch.load("../data/trained_models/{}.pth".format(conf['load_model']),
-                               map_location=torch.device(device))
-            except FileNotFoundError:
-                model = torch.load("trained_models/{}.pth".format(conf['load_model']),
-                                   map_location=torch.device(device))
+    # custom_encoder = VGGExtractor().to(conf['device'])
+    # https://pytorch.org/docs/stable/generated/torch.nn.Transformer.html
+    model = torch.nn.Transformer(d_model=40, nhead=8, num_encoder_layers=6,
+                                 num_decoder_layers=6)  # , custom_encoder=custom_encoder) TODO: encoder not working!!!
 
-    else:
-        # custom_encoder = VGGExtractor().to(conf['device'])
-        # https://pytorch.org/docs/stable/generated/torch.nn.Transformer.html
-        model = torch.nn.Transformer(d_model=40) #, custom_encoder=custom_encoder) TODO: encoder not working!!!
+    # print(model)
+    print("Model has {} parameters".format(count_parameters(model)))
 
-        print(model)
-        print("Model has {} parameters".format(count_parameters(model)))
+    if 'load_weights' in conf and conf['load_weights'] != 'None':
+        load_weights(conf, model, device)
 
-    #  just for "old" models using DataParallel (now using DistributedDataParallel)
-    if isinstance(model, torch.nn.DataParallel):
-        model = model.module
+    return model.to(device)
 
-    return model
+
+def load_weights(net_config, model, rank=None):
+    name = "{}.pth".format(net_config['load_weights'])
+    mapping = torch.device(net_config['device']) if not isinstance(rank, int) else {'cuda:%d' % 0: 'cuda:%d' % rank}
+
+    state_dict = torch.load("/workspace/data_pa/trained_models/{}".format(name), map_location=mapping)
+
+    state_dict_single = {}
+    for k, v in state_dict.items():
+        if k.startswith('module.'):
+            state_dict_single[k[7:]] = v
+        else:
+            state_dict_single[k] = v
+
+    model.load_state_dict(state_dict)
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)

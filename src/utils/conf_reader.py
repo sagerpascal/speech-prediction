ff38d119ea78ac739d91a08e096098714c1c2640
Pascal Sager <sage@zhaw.ch>
2021-03-29 19:23:02 +0200

    Mode in arguments

--- src/utils/conf_reader.py
+++ src/utils/conf_reader.py
@@ -25,17 +25,19 @@ def get_config():
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", default='train', help="'train' or 'eval'")
     parser.add_argument("--learning_rate", default=0.0005, help="The learning rate")
     parser.add_argument("--weight_decay", default=0.0001, help="Weight decay of the optimizer")
     parser.add_argument("--load_weights", default=None, help="name of the model to load")
     parser.add_argument("--batch_size", default=64, help="The mini-batch size")
-    parser.add_argument("--step_size", default=10, help="LR scheduler step size")
+    parser.add_argument("--step_size", default=20, help="LR scheduler step size")
     parser.add_argument("--gamma", default=0.8, help="LR scheduler gamma")
     args = parser.parse_args()
 
     args_dict = {
         'load_weights': str(args.load_weights),
         'device': device,
+        'mode': str(args.mode),
     }
 
     conf = {**conf_file, **args_dict}

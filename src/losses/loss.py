0a377367b147487e5aa9257c3856ead60029eb9f
Pascal Sager <paes.sager@gmail.com>
2021-04-11 15:05:25 +0200

    Check size in loss function

--- src/losses/loss.py
+++ src/losses/loss.py
@@ -5,10 +5,24 @@ from losses.soft_dtw import SoftDTW
 
 class SoftDTWWrapper(SoftDTW):
 
-    def __init__(self, conf):
-        super().__init__(use_cuda=conf['device'] == "cuda")
+    def __init__(self, conf, gamma=1., length=None):
+        super().__init__(use_cuda=conf['device'] == "cuda", gamma=gamma)
+        self.conf = conf
+        self.bs = conf['train']['batch_size']
+        self.n_features = conf['data']['transform']['n_mels'] if conf['data']['type'] == 'mel-spectro' else conf['data']['transform']['n_mfcc']
+        if length is not None:
+            self.length = length
+        else:
+            self.length = conf['masking']['k_frames']
 
     def __call__(self, x, y):
+        # x and y should be batch_size x seq_len x dims
+
+        assert x.shape[1] == self.length
+        assert y.shape[1] == self.length
+        assert x.shape[2] == self.n_features
+        assert y.shape[2] == self.n_features
+
         return super().__call__(x, y).mean()
 
 

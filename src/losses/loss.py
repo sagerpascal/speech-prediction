93314c18ebfe4b8048a0d4f1c342715e687e78b9
Pascal Sager <paes.sager@gmail.com>
2021-04-18 15:23:29 +0200

    Add L1 loss

--- src/losses/loss.py
+++ src/losses/loss.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 from losses.soft_dtw import SoftDTW
@@ -9,7 +10,8 @@ class SoftDTWWrapper(SoftDTW):
         super().__init__(use_cuda=conf['device'] == "cuda", gamma=gamma)
         self.conf = conf
         self.bs = conf['train']['batch_size']
-        self.n_features = conf['data']['transform']['n_mels'] if conf['data']['type'] == 'mel-spectro' else conf['data']['transform']['n_mfcc']
+        self.n_features = conf['data']['transform']['n_mels'] if conf['data']['type'] == 'mel-spectro' else \
+            conf['data']['transform']['n_mfcc']
         if length is not None:
             self.length = length
         else:
@@ -26,9 +28,46 @@ class SoftDTWWrapper(SoftDTW):
         return super().__call__(x, y).mean()
 
 
+class WeightedL1Loss(torch.nn.L1Loss):
+
+    def __init__(self, conf):
+        super(WeightedL1Loss, self).__init__(reduction='none')
+        self.conf = conf
+        self.batch_size = self.conf['train']['batch_size']
+        self.n_dim = conf['data']['transform']['n_mfcc'] if conf['data']['type'] == 'mfcc' else \
+            conf['data']['transform']['n_mels']
+        self.seq_len = self.conf['masking']['n_frames']
+        self.weight = self.calc_weights(
+            batch_size = self.batch_size,
+            seq_length=self.seq_len,
+            n_dim=self.n_dim,
+        )
+
+    def calc_weights(self, batch_size, seq_length, n_dim):
+        weight_per_time = np.linspace(2, 0.5, seq_length)
+        weight_per_time_dimensions = np.repeat(weight_per_time[:, np.newaxis], n_dim, axis=1)
+        weights = np.repeat(weight_per_time_dimensions[np.newaxis, :, :], batch_size, axis=0)
+        return torch.as_tensor(weights, dtype=torch.float)
+
+    def forward(self, input, target):
+        raise NotImplementedError("Test me")
+        loss = super().forward(input, target)
+        if input.shape[0] == self.batch_size:
+            loss *= self.weight
+        else:
+            # only for the last batch if it contains less elements than batch_size
+            weights = self.calc_weights(input.shape[0], self.seq_length, self.n_dim)
+            loss *= weights
+        return torch.mean(loss)
+
+
 def get_loss(conf):
     if conf['train']['loss'] == 'mse':
         return torch.nn.MSELoss()
+    elif conf['train']['loss'] == 'mae':
+        return torch.nn.L1Loss()
+    elif conf['train']['loss'] == 'mae-weighted':
+        return WeightedL1Loss(conf)
     elif conf['train']['loss'] == 'soft-dtw':
         return SoftDTWWrapper(conf)
     else:

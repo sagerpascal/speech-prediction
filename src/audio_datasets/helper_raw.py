dd60438a0a80dc6a60cb4c9dd127f9a4099790b4
Pascal Sager <sage@zhaw.ch>
2021-05-11 21:10:42 +0200

    raw h5 files for efficiency

--- src/audio_datasets/helper_raw.py
+++ src/audio_datasets/helper_raw.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pathlib import Path
 
 import h5pickle
@@ -12,29 +13,25 @@ from tqdm import tqdm
 from utils.conf_reader import get_config
 
 
-def add_to_file(i, signal_start, signal, speaker, filename, signal_d, speaker_d, filepath_d, meta_d):
-    signal_end = signal_start + signal.shape[1]
-    signal_d.resize((1, signal_end))
-    signal_d[:, signal_start:signal_end] = signal
+def add_to_file(i, signal, speaker, filename, signal_d, speaker_d, filepath_d, meta_d):
+    signal_d[i, :signal.shape[1]] = signal.numpy()
     speaker_d[i, :] = speaker.encode("ascii")
     filepath_d[i, :] = filename.encode("ascii")
-    meta_d[i, :] = np.array([signal_start, signal_end])
-    return signal_end
+    meta_d[i, :] = np.array([signal.shape[1]])
 
 
-def create_h5_file_timit(orig_fp, meta_fp, h5_name):
+def create_h5_file_timit(orig_fp, meta_fp, h5_name, n_samples):
     base_path = Path('audio_datasets/dfs')
     df = pd.read_csv(base_path / orig_fp)
     number_of_entries = len(df)
 
     f_h5 = h5py.File(h5_name, 'w', libver='latest')
-    dataset = f_h5.create_dataset('RAW', (1, 1), maxshape=(1, None), chunks=True, dtype='float32')
+    dataset = f_h5.create_dataset('RAW', (n_samples, 124621), chunks=True, dtype='float32')
     speaker_dataset = f_h5.create_dataset('Speaker', (number_of_entries, 1), 'S7')
     filepath_dataset = f_h5.create_dataset('Filepath', (number_of_entries, 1), 'S30')
-    meta_dataset = f_h5.create_dataset('META', (number_of_entries, 2), dtype='int64')
+    meta_dataset = f_h5.create_dataset('META', (number_of_entries, 1), dtype='int64')
 
-    raw_start = 0
-    indexes, raw_starts, raw_ends, raw_lengths, speakers = [], [], [], [], []
+    indexes, raw_lengths, speakers = [], [], []
 
     for index, row in tqdm(df.iterrows(), total=len(df)):
         waveform = torchaudio.load(row['file_path'])
@@ -43,21 +40,14 @@ def create_h5_file_timit(orig_fp, meta_fp, h5_name):
         filename = row['file_path'][len('/workspace/data_pa/TIMIT/AUDIO_FILES/ORIGINAL/'):]
 
         indexes.append(index)
-        raw_starts.append(raw_start)
         raw_lengths.append(signal.shape[1])
         speakers.append(speaker)
 
-        raw_end = add_to_file(index, raw_start, signal.cpu(), speaker, filename,
-                              dataset, speaker_dataset, filepath_dataset,
-                              meta_dataset)
-
-        assert raw_end - raw_start == signal.shape[1]
-        raw_ends.append(raw_end)
-        raw_start = raw_end
+        add_to_file(index, signal.cpu(), speaker, filename,
+                    dataset, speaker_dataset, filepath_dataset,
+                    meta_dataset)
 
     df_meta = pd.DataFrame.from_dict({
-        'raw_start': raw_starts,
-        'raw_end': raw_ends,
         'raw_length': raw_lengths,
         'Speaker': speakers
     })
@@ -67,6 +57,7 @@ def create_h5_file_timit(orig_fp, meta_fp, h5_name):
     df['raw_length'] = raw_lengths
     df.to_csv(base_path / orig_fp, index=False)
 
+    f_h5.close()
 
 def calc_mean_std():
     h5_file = h5pickle.File('timit_raw_train.h5', 'r', skip_cache=False)
@@ -98,10 +89,34 @@ if __name__ == '__main__':
     os.chdir('../')
     conf = get_config()
     # check_h5_files()
-    # create_h5_file_timit('timit-orig-train.csv', 'timit_raw_metadata_train.csv',
-    #                      'timit_raw_train.h5')
-    # create_h5_file_timit('timit-orig-val.csv', 'timit_raw_metadata_valid.csv',
-    #                      'timit_raw_valid.h5')
-    # create_h5_file_timit('timit-orig-test.csv', 'timit_raw_metadata_test.csv',
-    #                      'timit_raw_test.h5')
-    calc_mean_std()
+    create_h5_file_timit('timit-orig-train.csv', 'timit_raw_metadata_train.csv',
+                         'timit_raw_train.h5', 3927)
+    create_h5_file_timit('timit-orig-val.csv', 'timit_raw_metadata_valid.csv',
+                         'timit_raw_valid.h5', 693)
+    create_h5_file_timit('timit-orig-test.csv', 'timit_raw_metadata_test.csv',
+                         'timit_raw_test.h5', 1680)
+    # calc_mean_std()
+
+    # h5_file2 = h5pickle.File('timit_raw_valid.h5', 'r', skip_cache=False)
+    #
+    # t1 = time.time()
+    # data = h5_file2['RAW'][692, :48640]  # size=204'917'204
+    # t2 = time.time()
+    #
+    # h5_file = h5pickle.File('/workspace/data_pa/TIMIT/timit_raw_valid-128.h5', 'r', skip_cache=False)
+    #
+    # t3 = time.time()
+    # data2 = h5_file['RAW'][:, 34060474:34109114]  # size=409'608'927
+    # t4 = time.time()
+    #
+    # df1 = pd.read_csv('audio_datasets/dfs/timit_raw_metadata_valid-128.csv')
+    # df2 = pd.read_csv('audio_datasets/dfs/timit_raw_metadata_valid.csv')
+    #
+    # for i in range(693):
+    #     l1, s1, e1, = df1['raw_length'][i], df1['raw_start'][i], df1['raw_end'][i]
+    #     l2 = df2['raw_length'][i]
+    #     data = h5_file['RAW'][:, s1:e1]
+    #     data2 = h5_file2['RAW'][i, :l2]
+    #     assert np.all(data == data2)
+    #
+    # print("{}, {}".format(t2-t1, t4-t3))
